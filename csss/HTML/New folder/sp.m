5202d486147475ca5a70c0beca906deb9cbd2bc4
Satya-prakash3 <satyaprakashsethy905@gmail.com>
2022-08-20 02:19:08 +0530

    first commit

---
+++ csss/HTML/New folder/sp.m
@@ -0,0 +1,6 @@
+
+ TRY
+          statement, ..., statement, 
+       catch ME
+          statement, ..., statement 
+       END
---
+++ csss/New Text Document (1).php
@@ -0,0 +1,5 @@
+<?php
+$Name="sushma";
+echo "my name is $Name";
+echo 'my name is $Name';
+?>
